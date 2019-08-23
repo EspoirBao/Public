d02d6d308fa90bf0eb02b34cb6ef79141de16dae
EspoirBao <51221361+EspoirBao@users.noreply.github.com>
2019-08-23 15:57:26 +0800

    post
    v1.0.9

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,4 @@
+// 鼠标移入功能——————————————————————————————————————————————————————————————
 $(document).on('mouseover','#tools_box1',function(){
     $('#tools_message').show()
     $(document).on('mouseleave','#tools_box1',function(){
@@ -12,24 +13,6 @@ $(document).on('mouseover','#tools_message',function(){
     })
 })
 
-
-$(document).on('click','#tools_message_button',function(){
-    location.href="./html/login.html";
-})
-
-if(getCookie('User_name')){
-    var User_name=getCookie('User_name')
-    $('#tools_line1').html(function(){
-        return '您好,'+User_name
-     })
-     $('#nav_a a').first().html(function(){
-         return User_name+'的亚马逊'
-     })
-     $('#no_login').hide()
-}else{
-    
-}
-
 $(document).on('mouseover','#nav_line_box',function(){
     $('#nav_content').show();
     $(document).on('mouseleave','#nav_line_box',function(){
@@ -43,4 +26,35 @@ $(document).on('mouseover','#nav_content',function(){
         $('#nav_content').hide()
     })
 })
+
+// 鼠标移入功能——————————————————————————————————————————————————————————————
+
+
+
+
+// 用户登陆判断——————————————————————————————————————————————————————
+$(document).on('click','#tools_message_button',function(){
+    location.href="./html/login.html";
+})
+
+if(getCookie('User_name')){
+    var User_name=getCookie('User_name')
+    $('#tools_line1').html(function(){
+        return '您好,'+User_name
+     })
+     $('#nav_a a').first().html(function(){
+         return User_name+'的亚马逊'
+     })
+     $('#no_login').hide()
+     $('#tools_text').show()
+}else{
+    $('#no_login').show()
+     $('#tools_text').hide()
+}
+$(document).on('click','.Exit_account',function(){
+    removeCookie('User_name')
+})
+
+
+// 用户登陆判断——————————————————————————————————————————————————————
 
